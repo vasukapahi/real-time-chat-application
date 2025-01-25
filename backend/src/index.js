586a1cbd83216d8c1d63faa57ab6dc5d11785092
vasu kapahi <vasukapahi16@gmail.com>
2025-01-25 15:40:45 +0530

    backend authentication and message send receive routes

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+import cookieParser from"cookie-parser";
+dotenv.config();
+const app=express();
+
+const PORT=process.env.PORT;  
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth",authRoutes); 
+app.use("/api/message",messageRoutes); 
+  
+app.listen(PORT,()=>{
+    console.log("Server is running on port:"+PORT);
+    connectDB();
+})
---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getUsersForSidebar,getMessages,sendMessages } from "../controllers/message.controllers.js";
+const router=express.Router();
+router.get("/users",protectRoute,getUsersForSidebar);
+router.get("/:id",protectRoute,getMessages);
+router.post("/send/:id",protectRoute,sendMessages);
+
+export default router;
