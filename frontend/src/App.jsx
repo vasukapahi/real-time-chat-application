52e485c27a2e3f04907722e6cced7459e1b57a2c
vasu kapahi <vasukapahi16@gmail.com>
2025-01-27 23:57:00 +0530

    fixed the tailwind and daisy ui error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,7 @@
 const App=()=>{
-  return <div className="text-red-500">Hello World</div>
+  return <div className="text-red-500"><button className="btn btn-info">Info</button>
+  <button className="btn btn-success">Success</button>
+  <button className="btn btn-warning">Warning</button>
+  <button className="btn btn-error">Error</button></div>
 }
 export default App;
