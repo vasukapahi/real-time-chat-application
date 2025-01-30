0584085c5af3ed2183c2fbbdcdaeb29ada6fc2e9
vasu kapahi <vasukapahi16@gmail.com>
2025-01-30 23:07:07 +0530

    created all frontent routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import SettingsPage from "./pages/SettingsPage";
+import {Routes,Route} from "react-router-dom";
 const App=()=>{
-  return <div className="text-red-500"><button className="btn btn-info">Info</button>
-  <button className="btn btn-success">Success</button>
-  <button className="btn btn-warning">Warning</button>
-  <button className="btn btn-error">Error</button></div>
+  return <div >
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/signup" element={<SignUpPage/>}/>
+      <Route path="/login" element={<LoginPage/>}/>
+      <Route path="/settings" element={<SettingsPage/>}/>
+      <Route path="/profile" element={<ProfilePage/>}/>
+
+    </Routes>
+  </div>
 }
 export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Router } from 'react-router-dom'
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
