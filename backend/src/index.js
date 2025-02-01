5fc4c12e3bf60195487b9ba9b3af26b45a4461c9
vasu kapahi <vasukapahi16@gmail.com>
2025-02-02 01:55:12 +0530

    managing navigations and route protections using custom hooks

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,13 +4,19 @@ import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from"cookie-parser";
+import cors from "cors";
 dotenv.config();
 const app=express();
 
 const PORT=process.env.PORT;  
 app.use(express.json());
 app.use(cookieParser());
-
+app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+    })
+  );
 app.use("/api/auth",authRoutes); 
 app.use("/api/message",messageRoutes); 
   
